98a618e11b357db5418be0a7efa42512fc06e12f
Pedro Henrique <pedro.henriquy.dev@gmail.com>
2026-09-23 09:31:23 -0300

    Update Ex 4.c

--- Estruturas/Ex 4.c
+++ Estruturas/Ex 4.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 
-struct Hora{
-int hora;
-int minuto;
-int segundo;
+struct RegHora{
+int h;
+int m;
+int s;
 };
 
 int main(){
-struct Hora h[5];
-int i,maior=0;
+struct RegHora lista[5];
+int i,maiorPos=0;
 
 for(i=0;i<5;i++){
-printf("Hora %d\n",i+1);
+printf("Horario %d\n",i+1);
 
-printf("Hora: ");
-scanf("%d",&h[i].hora);
+printf("Horas: ");
+scanf("%d",&lista[i].h);
 
-printf("Minuto: ");
-scanf("%d",&h[i].minuto);
+printf("Minutos: ");
+scanf("%d",&lista[i].m);
 
-printf("Segundo: ");
-scanf("%d",&h[i].segundo);
+printf("Segundos: ");
+scanf("%d",&lista[i].s);
 }
 
 for(i=1;i<5;i++){
-if(h[i].hora>h[maior].hora)
-maior=i;
-else if(h[i].hora==h[maior].hora && h[i].minuto>h[maior].minuto)
-maior=i;
-else if(h[i].hora==h[maior].hora && h[i].minuto==h[maior].minuto && h[i].segundo>h[maior].segundo)
-maior=i;
+if(lista[i].h>lista[maiorPos].h)
+maiorPos=i;
+else if(lista[i].h==lista[maiorPos].h && lista[i].m>lista[maiorPos].m)
+maiorPos=i;
+else if(lista[i].h==lista[maiorPos].h && lista[i].m==lista[maiorPos].m && lista[i].s>lista[maiorPos].s)
+maiorPos=i;
 }
 
-printf("Maior hora: %02d:%02d:%02d\n",h[maior].hora,h[maior].minuto,h[maior].segundo);
+printf("Maior horario digitado: %02d:%02d:%02d\n",lista[maiorPos].h,lista[maiorPos].m,lista[maiorPos].s);
 
 return 0;
 }
