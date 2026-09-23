aab9aa43f976dcd3be55b2348de90b7d0b906318
Pedro Henrique <pedro.henriquy.dev@gmail.com>
2026-09-23 09:34:21 -0300

    Update Ex 3.c

--- Ponteiros_lista3/Ex 3.c
+++ Ponteiros_lista3/Ex 3.c
@@ -2,20 +2,20 @@
 
 int main(){
 
-    int matriz[3][3]={
+    int m[3][3]={
         {1,2,3},
         {4,5,6},
         {7,8,9}
     };
 
-    int *ptr=&matriz[0][0];
-    int soma=0;
+    int *p=&m[0][0];
+    int total=0;
 
-    soma = *ptr;
-    soma = soma + *(ptr+4);
-    soma = soma + *(ptr+8);
+    total = *p;
+    total = total + *(p+4);
+    total = total + *(p+8);
 
-    printf("Soma = %d",soma);
+    printf("Soma da diagonal: %d",total);
 
     return 0;
 }
