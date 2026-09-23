7cf90609a891e0a815ec20dc005bad186f74d3b9
Pedro Henrique <pedro.henriquy.dev@gmail.com>
2026-09-23 09:31:57 -0300

    Update Ex 6.c

--- Estruturas/Ex 6.c
+++ Estruturas/Ex 6.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-struct Atleta{
+struct FichaAtleta{
 char nome[50];
-char esporte[50];
+char modalidade[50];
 int idade;
 float altura;
 };
 
 int main(){
-struct Atleta a[5];
-int i,maisAlto=0,maisVelho=0;
+struct FichaAtleta a[5];
+int i,iAlto=0,iVelho=0;
 
 for(i=0;i<5;i++){
 printf("Atleta %d\n",i+1);
@@ -18,7 +18,7 @@ printf("Nome: ");
 scanf(" %[^\n]",a[i].nome);
 
 printf("Esporte: ");
-scanf(" %[^\n]",a[i].esporte);
+scanf(" %[^\n]",a[i].modalidade);
 
 printf("Idade: ");
 scanf("%d",&a[i].idade);
@@ -28,15 +28,15 @@ scanf("%f",&a[i].altura);
 }
 
 for(i=1;i<5;i++){
-if(a[i].altura>a[maisAlto].altura)
-maisAlto=i;
+if(a[i].altura>a[iAlto].altura)
+iAlto=i;
 
-if(a[i].idade>a[maisVelho].idade)
-maisVelho=i;
+if(a[i].idade>a[iVelho].idade)
+iVelho=i;
 }
 
-printf("Mais alto: %s\n",a[maisAlto].nome);
-printf("Mais velho: %s\n",a[maisVelho].nome);
+printf("Mais alto: %s\n",a[iAlto].nome);
+printf("Mais velho: %s\n",a[iVelho].nome);
 
 return 0;
 }
